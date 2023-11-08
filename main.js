8ef9d5c768d4adffc134772ca5cb64d468d4385a
Masterchef842 <robertwilliamlord55@gmail.com>
2023-11-08 15:17:25 -0600

    factory functions and whatnot

--- main.js
+++ main.js
@@ -12,10 +12,76 @@ const mockUpStrand = () => {
   }
   return newStrand;
 };
+const idList=[];
+function pAequorFactory(){
+  let id= Math.floor(Math.random()*1000)+1;
+  while(idList.includes(id)){
+    id= Math.floor(Math.random()*1000)+1;
+  }
+  idList.push(id);
+
+  return {
+    specimenNum: id,
+
+    dna: mockUpStrand(),
+
+    mutate(){
+      let randIndex=Math.floor(Math.random()*this.dna.length);
+      let newBase= returnRandBase();
+      while(newBase===this.dna[randIndex]){
+        newBase= returnRandBase();
+      }
+      this.dna[randIndex]=newBase;
+      return this.dna;
+    },
+
+    compareDNA(dna2){
+      let count=0;
+      for(let i=0;i<this.dna.length;i++){
+        if(this.dna[i]===dna2[i]){
+          count++;
+        }
+      }
+
+      let percent= Math.floor((count/this.dna.length)*100);
+      console.log(`specimen #1 and specimen #2 have ${percent}% DNA in common`);
+    }
+  }
+}
 
 
+//creation tests
+
+let t1=pAequorFactory();
+let t2=pAequorFactory();
+let t3=pAequorFactory();
+let t4=pAequorFactory();
+let t5=pAequorFactory();
 
 
+//id test
+// console.log(t1.specimenNum);
+// console.log(t2.specimenNum);
+// console.log(t3.specimenNum);
+// console.log(t4.specimenNum);
+// console.log(t5.specimenNum);
+
+
+//mutate test
+// console.log(t1.dna);
+// t1.mutate();
+// console.log(t1.dna);
+// t1.mutate();
+// console.log(t1.dna);
+// t1.mutate();
+// console.log(t1.dna);
+// t1.mutate();
+// console.log(t1.dna);
+
+//compare test
+// t1.compareDNA(t2.dna);
+// t1.mutate();
+// t1.compareDNA(t2.dna);
 
 
 
